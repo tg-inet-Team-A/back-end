dd5beb27c0372b9b1e3065dbce435bf089d5ac1d
KatoYugo <kato.1999.cs@gmail.com>
2020-11-21 18:16:29 +0900

    地域ごとのカウント処理完成

--- main.py
+++ main.py
@@ -19,16 +19,41 @@ con.commit()
 cur.close() 
 con.close()
 
+# 仮データの追加処理
+# con = sqlite3.connect(dbname)
+# cur = con.cursor()
+# insert = 'insert into homes(id, erea_id, wake_up) values (?,?,?)'
+# cur.execute(insert, (0, 0, 0))
+# cur.execute(insert, (1, 0, 1))
+# cur.execute(insert, (2, 0, 1))
+# cur.execute(insert, (3, 1, 0))
+# cur.execute(insert, (4, 1, 0))
+
+# insert = 'insert into ereas(id, name) values (?,?)'
+# cur.execute(insert, (0, "TOKYO"))
+# cur.execute(insert, (1, "Osaka"))
+
+# con.commit()
+# cur.close() 
+# con.close()
+
 @app.route('/erea/<erea_id>/wakeup_people', methods=["GET", "POST"])
-def count(erea_id=None, home_id =None):
+def count(erea_id=None, home_id =0):
+    erea_id = int(erea_id)
     con = sqlite3.connect(dbname)
     cur = con.cursor()
-    table = cur.execute("SELECT * FROM ereas WHERE id == erea_id")
-    num = 0
-    for i in table:
-        num += 1
 
-    table = cur.execute("UPDATE homes SET wake_up =1 WHERE id = home_id")
+    # カウント処理
+    sql = "SELECT COUNT(*) FROM homes WHERE erea_id == ? and wake_up == 1"
+    result = cur.execute(sql, (erea_id, ))
+    num = result.fetchone()
+    num = num[0]
+    
+    # 起床処理
+    sql = "UPDATE homes SET wake_up =1 WHERE id = ?"
+    cur.execute(sql, (home_id, ))
+    con.commit()
+
     cur.close() 
     con.close()
     return Response(response=json.dumps({'erea_id': erea_id, "wakeup_people": num}), status=500)
